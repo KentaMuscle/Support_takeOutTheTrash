6a0626dc04dd2a260d24e5047eab6ab2cfc76c19
KentaMuscle <kentawebcorder@gmail.com>
2019-09-07 14:09:56 +0900

    Implementation of login and member registration functions using devise(email authentication)

--- Gemfile
+++ Gemfile
@@ -60,3 +60,8 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Add by kenta okubo
+gem 'devise'
+gem 'omniauth'
+gem 'omniauth-google-oauth2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  root to: "home#index"
+
+  # deviseコントローラー
+  devise_for :users
+  resources :users
+
+  #homeコントローラー
+  get 'home', to: "home#index"
+  get 'home/index'
+  get 'home/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
